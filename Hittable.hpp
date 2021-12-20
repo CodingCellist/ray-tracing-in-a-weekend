df75ca251a7c4b601169da16196255f29031cc77
Thomas E. Hansen <teh6@st-andrews.ac.uk>
2021-12-20 17:20:52 +0100

    Implement `Hittable` abstract class

---
+++ Hittable.hpp
@@ -0,0 +1,21 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "Ray.hpp"
+
+struct hit_record {
+  // point of intersection
+  Point3 p;
+  // surface normal
+  Vec3 normal;
+  // `t` at which the hit occurred
+  double t;
+};
+
+class hittable {
+  public:
+    virtual bool hit(const Ray& r, double t_min, double t_max, hit_record& rec) const = 0;
+};
+
+#endif
+
