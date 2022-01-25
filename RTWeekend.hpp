c871fcee99326d9eec67c38c4425b3a8a7992f0a
Thomas E. Hansen <teh6@st-andrews.ac.uk>
2022-01-25 15:57:09 +0100

    Implement random no gen.n for [0,1[ and [min,max[.

--- RTWeekend.hpp
+++ RTWeekend.hpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 
 // USINGS //
@@ -23,6 +24,16 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
   return degrees * pi / 180.0;
+}
+
+// Return a random real in [0, 1[ .
+inline double random_double() {
+  return rand() / (RAND_MAX + 1.0);
+}
+
+// Return a random real in [min, max[ .
+inline double random_double(double min, double max) {
+    return min + (max - min) * random_double();
 }
 
 
