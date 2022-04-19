4d8efa885bc762bb9295c5fe72d09a75f414506d
Thomas E. Hansen <teh6@st-andrews.ac.uk>
2022-04-19 11:56:34 +0200

    Implement view direction and orientation
    If the camera points from lookFrom, looking at lookAt, then we can
    define a unit vector w based on the lookAt_lookFrom vector. Given an
    "up" direction (vup) for the camera, defining the camera's plane then
    becomes:
    - horiz. basis = w × vup
      * unit vector := u
    - vert. basis = w × u
      * unit vector := v
    And we can get the camera height and width by multiplying the viewport
    dimensions with these unit vectors respectively.
    
    ... I remember a surprising amount of vector calc...

--- Camera.hpp
+++ Camera.hpp
@@ -8,6 +8,9 @@ class Camera {
     // CONSTRUCTORS //
 
     Camera(
+        Point3 lookFrom,
+        Point3 lookAt,
+        Vec3 vup,
         double vfov,    // vertical field-of-view, in degrees
         double aspect_ratio
     ) {
@@ -18,20 +21,23 @@ class Camera {
       auto viewport_height = 2.0 * h;
       auto viewport_width = aspect_ratio * viewport_height;
 
-      auto focal_length = 1.0;
+      // orthonormal basis for camera orientation
+      auto w = unit_vector(lookFrom - lookAt);  // we point the camera at -w
+      auto u = unit_vector(cross(vup, w));      // horiz. axis is vup × w
+      auto v = cross(w, u);                     // vert. axis is w × u
 
-      origin = Point3(0, 0, 0);   // camera "sees" from 0,0,0
-      horizontal = Vec3(viewport_width, 0.0, 0.0);  // y is horizontal
-      vertical = Vec3(0.0, viewport_height, 0.0);   // x is vertical
-      lower_left_corner =
-        origin - horizontal / 2 - vertical / 2 - Vec3(0, 0, focal_length);
+      origin = lookFrom;                // camera "sees" from `lookFrom` point
+      horizontal = viewport_width * u;  // horiz. is: width * horiz. basis vect.
+      vertical = viewport_height * v;   // vert. is: width * vert. basis vect.
+      lower_left_corner = origin - horizontal / 2 - vertical / 2 - w;
     }
 
     // METHODS //
 
-    Ray get_ray(double u, double v) const {
+    Ray get_ray(double s, double t) const {
       return Ray( origin
-                , lower_left_corner + u * horizontal + v * vertical - origin);
+                , lower_left_corner + s * horizontal + t * vertical - origin
+                );
     }
 
   private:
